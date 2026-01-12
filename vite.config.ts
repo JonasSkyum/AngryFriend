fcf28b4a3415c3e53feee80bb87de9e9ef299d4c
Jonas Skyum <js@brandbyhand.dk>
2026-01-12 20:06:58 +0100

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/AngryFriend/',
   server: { port: 5173 },
   build: {
     target: 'es2020'
